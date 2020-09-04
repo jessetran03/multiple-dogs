3421fbe79f11fb32e885855229ad4fbb45c81c14
Jesse Tran <jessetran03@gmail.com>
2020-09-03 21:18:11 -0500

    updated files

--- app.js
+++ app.js
@@ -1,33 +1,45 @@
 'use strict';
 
 function getNumber() {
-    const userNumber = $('#number').val();
+    let userNumber = $('#number').val();
+    if (!userNumber) {
+        userNumber = 3;
+    }
     return userNumber;
 }
 
 function getDogImage(number) {
-    for (let i = 0; i < number; i++)
-    fetch('https://dog.ceo/api/breeds/image/random')
+    fetch(`https://dog.ceo/api/breeds/image/random/${number}`)
         .then(response => response.json())
         .then(responseJson => 
-            displayImage(responseJson))
+            displayImage(responseJson, number))
         .catch(error => alert('Something went wrong. Try again later.'));
 }
 
-function displayImage(responseJson) {
+function displayImage(responseJson, number) {
     console.log(responseJson);
-    $('.results').append(
-        `<img src="${responseJson.message}" class="results-img">`
-    );
+    for (let i = 0; i < number; i ++) {
+        $('.results').append(
+            `<img src="${responseJson.message[i]}" class="results-img">`
+        );
+    }
 }
 
 function watchForm() {
     $('form').submit(event => {
         event.preventDefault();
-        $('.results').empty().append(
-            `<h3>So many dogs!!!</h3>`
-        );
-        getDogImage(getNumber());
+        const number = getNumber();
+        if (number > 0 && number <= 50) {
+            $('.results').empty().append(
+                `<h3>So many dogs!!!</h3>`
+            );
+            getDogImage(number);
+            }
+        else {
+            $('.results').empty().append(
+                `<p>Please enter a number between 1 and 50.<p>`
+            );
+        }
     });
 }
 
